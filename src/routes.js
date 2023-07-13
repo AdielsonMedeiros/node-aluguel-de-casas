44edbad5c176aa908c38e32c6f7aad18c751076d
Adielson Medeiros <medeirosadielson@gmail.com>
2023-07-13 00:57:08 -0300

    dashboard

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import uploadConfig from "./config/upload"
 import SessionController from "./controllers/SessionController";
 import HouseController from "./controllers/HouseController";
 import House from "./models/House";
+import DashboardController from "./controllers/DashboardController";
 
 const routes = new Router();
 
@@ -14,6 +15,7 @@ routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.get('/houses', HouseController.index);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
+routes.get('/dashboard', DashboardController.show);
 
 
 export default routes;
