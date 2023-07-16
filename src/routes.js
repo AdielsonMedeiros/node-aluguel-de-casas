a2a2e3216467305288fa936a9f7516ee3da8b63e
Adielson Medeiros <medeirosadielson@gmail.com>
2023-07-16 01:32:51 -0300

    listando e excluindo reserva

--- src/routes.js
+++ src/routes.js
@@ -17,8 +17,10 @@ routes.get('/houses', HouseController.index);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
 routes.get('/dashboard', DashboardController.show);
-
 routes.post('/houses/:house_id/reserve', ReserveController.store);
+
+routes.get('/reserves', ReserveController.index);
+routes.delete('/reserves/cancel', ReserveController.destroy);
 
 
 export default routes;
